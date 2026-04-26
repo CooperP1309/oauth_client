5a6c8dcc8a5d326e8aebcc0e35edc5fff468e770
CooperP1309 <cooperpeek1309@gmail.com>
2026-04-26 20:01:27 +1000

    Add project files.

---
+++ oauth_client/external/platform.hpp
@@ -0,0 +1,194 @@
+#ifndef UNIQUE_HEADER_NAME_H
+#define UNIQUE_HEADER_NAME_H
+
+#include <string>
+#include <filesystem>
+#include <fstream>
+
+/* External Libs */
+#define CPPHTTPLIB_MBEDTLS_SUPPORT
+#include "external/httplib.h"
+#include "external/json.hpp"
+
+#pragma comment(lib, "bcrypt.lib")
+namespace fs = std::filesystem;
+using json = nlohmann::json;
+
+class Platform 
+{
+public:
+
+	Platform(std::string);
+
+private: 
+    std::string host;
+    std::string client_id;
+	std::string client_secret;
+	std::string scope;
+	std::string token_endpoint;
+
+	/* Initializer Functions */
+    int extract_credentials(std::string);
+	int assign_value(std::string, std::string);
+    int get_access_token(std::string&);
+
+    /* API Functions */
+
+};
+
+Platform::Platform(std::string platform_name) 
+/*
+*   Constructor initializes platform credentials for API calls.
+* 
+*   Reads credentials in client_credentials.txt file and assigns member variables to key-value 
+*   pairs associated with the provided platform name. Member variables are then formatted for 
+*   an access token request. Which if successful, fully initializes the class for use in API calls.
+*/
+{
+    if (extract_credentials(platform_name)) 
+    {
+	    std::cerr << "Failed to extract credentials for platform: '" << platform_name << "'" << std::endl;
+        return;
+    }
+
+	std::cout << "Extracted following credentials for platform: '" << platform_name << "'" << std::endl << std::endl;
+	std::cout << "\tclient_id: " << client_id << std::endl;
+	std::cout << "\tclient_secret: " << client_secret << std::endl;
+	std::cout << "\tscope: " << scope << std::endl;
+	std::cout << "\ttoken_endpoint: " << token_endpoint << std::endl;
+	std::cout << "\thost: " << host << std::endl << std::endl;
+
+    std::string result_buffer;
+	if (get_access_token(result_buffer)) 
+    {
+        std::cerr << "Failed to get access token for platform: '" << platform_name << "'" << std::endl;
+        return;
+    }
+
+	std::cout << "Successfully initialized platform: '" << platform_name << "'" << std::endl << std::endl;    
+    std::cout << "\tAccess Token: " << result_buffer << std::endl << std::endl;
+}
+
+int Platform::extract_credentials(std::string platform_name)
+{
+    /* simplify space stripping by enforcing name.key standard */
+    platform_name = platform_name + ".";
+    fs::path file_path = fs::current_path() / "client_credentials.txt";
+
+    std::ifstream file(file_path);
+    if (file.is_open())
+    {
+        std::string line;
+        while (std::getline(file, line))
+        {
+			if (line.starts_with(platform_name))
+            {
+            
+                std::string key_value = line.substr(platform_name.size());
+                size_t delimiter_pos = key_value.find('=');
+
+                if (delimiter_pos != std::string::npos)
+                {
+                    std::string key = key_value.substr(0, delimiter_pos);
+                    std::string value = key_value.substr(delimiter_pos + 1);
+                    value.erase(value.find_last_not_of(" \t") + 1);
+
+                    if (assign_value(key, value))
+                    {
+                        return -1;
+                    }
+                }
+            }
+        }
+
+        file.close();
+
+        return 0;
+    }
+    else
+    {
+        std::cerr << "Could not open file: " << file_path << std::endl;
+        return -1;
+    }
+}
+
+int Platform::assign_value(std::string key, std::string value)
+{
+    if (key == "client_id")
+    {
+        client_id = value;
+    }
+    else if (key == "client_secret")
+    {
+		client_secret = value;
+    }
+    else if (key == "scope")
+    {
+		scope = value;
+    }
+    else if (key == "token_endpoint")
+    {
+		token_endpoint = value;
+    }
+    else if (key == "host")
+    {
+		host = value;
+    }
+    else {
+        std::cerr << "Unknown key: " << key << std::endl;
+		return -1;
+    }
+
+    return 0;
+}
+
+
+int Platform::get_access_token(std::string &result_buffer)
+/*  
+*   Specialized request client for getting access token.
+*
+*   Uses extracted client credentials build a POST request.
+*   This request is then sent to the provided endpoint to
+*   get an access token.
+*/
+{
+	if (host.empty() || client_id.empty() || client_secret.empty() || scope.empty() || token_endpoint.empty()) 
+    {
+        std::cerr << "Missing required credentials for access token request." << std::endl;
+        return -1;
+    }
+
+    httplib::SSLClient cli(host, 443);
+    cli.enable_server_certificate_verification(true);
+
+    httplib::Params params;
+    params.emplace("grant_type", "client_credentials");
+    params.emplace("client_id", client_id);
+    params.emplace("client_secret", client_secret);
+    params.emplace("scope", scope);
+    auto res = cli.Post(token_endpoint, params);
+
+    if (res && res->status == 200) 
+    {
+        std::cout << "POST Status: " << res->status << std::endl;
+		
+        /* Parse JSON response and extract Access Token */
+        result_buffer = res->body;
+		json response_json = json::parse(result_buffer);
+		result_buffer = response_json["access_token"].get<std::string>();
+
+		return 0;
+    }
+    else if (res) 
+    {
+		std::cerr << "HTTP Error: " << res->status << std::endl;
+    }
+    else 
+    {
+	    std::cerr << "HTTP Error: " << httplib::to_string(res.error()) << std::endl;
+    }
+    
+    return -1;
+}
+
+#endif // UNIQUE_HEADER_NAME_H
---
+++ oauth_client/oauth_client.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+/* external libs */
+#include "external/platform.hpp"
+
+int main()
+{
+	Platform platform("ninjaone");
+
+	std::cin.get();
+
+    return 0;
+}
